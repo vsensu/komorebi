57cc45b6e9c224c1883fb1f0775298a05279809b
vsensu <vsensu@foxmail.com>
2023-06-20 15:29:41 +0800

    Uniform buffers and a 3d camera

--- crates/komorebi_core/src/window.rs
+++ crates/komorebi_core/src/window.rs
@@ -61,29 +61,31 @@ impl Window {
                     ref event,
                     window_id,
                 } if window_id == window.id() => {
-                    match event {
-                        WindowEvent::CloseRequested
-                        | WindowEvent::KeyboardInput {
-                            input:
-                                KeyboardInput {
-                                    state: ElementState::Pressed,
-                                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                                    ..
-                                },
-                            ..
-                        } => *control_flow = ControlFlow::Exit,
-                        WindowEvent::Resized(physical_size) => {
-                            state.resize(*physical_size);
+                    if !state.input(event) {
+                        match event {
+                            WindowEvent::CloseRequested
+                            | WindowEvent::KeyboardInput {
+                                input:
+                                    KeyboardInput {
+                                        state: ElementState::Pressed,
+                                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                                        ..
+                                    },
+                                ..
+                            } => *control_flow = ControlFlow::Exit,
+                            WindowEvent::Resized(physical_size) => {
+                                state.resize(*physical_size);
+                            }
+                            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                                // new_inner_size is &&mut so we have to dereference it twice
+                                state.resize(**new_inner_size);
+                            }
+                            _ => (),
                         }
-                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                            // new_inner_size is &&mut so we have to dereference it twice
-                            state.resize(**new_inner_size);
-                        }
-                        _ => (),
                     }
                 }
                 Event::RedrawRequested(window_id) if window_id == window.id() => {
-                    // state.update();
+                    state.update();
                     match state.render() {
                         Ok(_) => {}
                         // Reconfigure the surface if lost
